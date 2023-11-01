6acf4959a0d9e24c73811fa4b2693c2bcb97c5fd
johannpa <johann_paque@hotmail.fr>
2023-11-01 17:44:07 +0100

    Create the project for the test

---
+++ WebAPI.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace WebAPI.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange - create a new instance of the class we want to test - this where you would prepare everything for the test, prepare for testing
+
+            //Act - this is where the method we are testing is executed
+
+            //Assert
+        }
+    }
+}
